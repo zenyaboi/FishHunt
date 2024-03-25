91bd57bb3ab4500ea0a005c2144301297496d626
Gabriel Franke <74662066+zenyaboi@users.noreply.github.com>
2024-03-25 09:43:19 -0300

    created fish placeholder and fishing variable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,37 +7,56 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float moveSpeed = 7f;
+
     // this is for knockback for the player
     public Vector2 forceToApply;
     public float forceDamping = 1.2f;
 
-    // Start is called before the first frame update
+    // variable for checking if the player is fishing
+    public bool isFishing = false;
+
     void Start()
     {
         // getting the rigidbody2D component from gameobject
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         // input variables
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        // getting the player's input value
-        Vector2 playerInput = new Vector2(horizontal, vertical).normalized;
+        // Checking if the player is not fishing
+        if (!isFishing) {
+            // getting the player's input value
+            Vector2 playerInput = new Vector2(horizontal, vertical).normalized;
 
-        // applying force to the player movement
-        Vector2 moveForce = playerInput * moveSpeed;
-        // in case the player gets hit, the players gets knocked back affecting its position
-        moveForce += forceToApply;
-        forceToApply /= forceDamping;
+            // applying force to the player movement
+            Vector2 moveForce = playerInput * moveSpeed;
 
-        if (Mathf.Abs(forceToApply.x) <= 0.01f && Mathf.Abs(forceToApply.y) <= 0.01f) {
-            forceToApply = Vector2.zero;
+            // in case the player gets hit, the players gets knocked back affecting its position
+            moveForce += forceToApply;
+            forceToApply /= forceDamping;
+            if (Mathf.Abs(forceToApply.x) <= 0.01f && Mathf.Abs(forceToApply.y) <= 0.01f)
+            {
+                forceToApply = Vector2.zero;
+            }
+
+            // applying the movement force to rigidbody's velocity
+            rb.velocity = moveForce;
+        } else {
+            rb.velocity = Vector2.zero;
         }
-        // applying the movement force to rigidbody's velocity
-        rb.velocity = moveForce;
+    }
+
+    private void OnTriggerStay2D(UnityEngine.Collider2D collision)
+    {
+        // Checking if the player is colliding with a gameobject with the tag Fish
+        if (collision.gameObject.tag == "Fish") {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                Debug.Log("Fish");
+            }
+        }
     }
 }
