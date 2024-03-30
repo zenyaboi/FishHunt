666654ca6827a99f367dfe7529a283a419ef3888
zenyaboi <bannedboyinlife@gmail.com>
2024-03-30 13:32:41 -0300

    created hook variables and regions for variables

--- Assets/Scripts/FishingMinigame.cs
+++ Assets/Scripts/FishingMinigame.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class FishingMinigame : MonoBehaviour
 {
+    #region Fish variables
     public GameObject fishing;
 
     [SerializeField] Transform topPivot, bottomPivot, fish;
@@ -12,11 +13,24 @@ public class FishingMinigame : MonoBehaviour
     float fishPosition;
     float fishDestination;
     float fishTimer;
-    [SerializeField] float timerMultiplicator = 3f;
+    [SerializeField] float timerMultiplicator = 3f; // higher the value faster the fish will go to the destination but will spend more time "stuck" in one place
 
     float fishSpeed;
     [SerializeField] float smoothMotion = 1f;
+    #endregion
 
+    #region Hook variables
+    [SerializeField] Transform hook;
+    float hookPosition;
+    [SerializeField] float hookSize = 0.1f;
+    [SerializeField] float hookPower = 5f;
+    float hookProgress;
+    float hookPullVelocity;
+    [SerializeField] float hookPullPower = 0.01f;
+    [SerializeField] float hookGravityPower = 0.005f;
+    [SerializeField] float hookProgressDegradation = 1f;
+    #endregion
+    
     private void Update() 
     {
         if (fishing.activeSelf) {
