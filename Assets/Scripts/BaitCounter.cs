8048e699cee59e49cc1dfd1a8a32e51f04f7e363
zenyaboi <bannedboyinlife@gmail.com>
2024-05-26 22:49:41 -0300

    added bait counter

---
+++ Assets/Scripts/BaitCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BaitCounter : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+
+    public int bait = 3;
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        text.text = "Bait: " + bait.ToString();
+    }
+}
